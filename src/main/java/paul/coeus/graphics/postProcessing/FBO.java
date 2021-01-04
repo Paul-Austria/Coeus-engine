c4c0dd4ce2fb577753c00973473f835f0c9cc410
KspPaul <1paulengelhardt@gmail.com>
2021-01-04 10:03:02 +0100

    closes #2

--- src/main/java/paul/coeus/graphics/postProcessing/FBO.java
+++ src/main/java/paul/coeus/graphics/postProcessing/FBO.java
@@ -1,40 +1,40 @@
 package paul.coeus.graphics.postProcessing;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.GL32;
 import paul.coeus.GlobalModules;
 import paul.coeus.graphics.Material.Texture;
 import paul.coeus.objects.Base.ShaderHandler.IShaderHandler;
 
 import java.nio.ByteBuffer;
 
-import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.opengl.GL32.glFramebufferTexture;
 
 public class FBO {
 
     private  int id;
-    private Texture texture;
+    private Texture colourTexture;
+    private int depthTexture;
+    private int depthBuffer;
     private FBOShaderHandler shaderHandler;
     FBOMesh mesh;
-    int depthBuffer ;
     public FBO() throws Exception {
 
-        glEnable(GL_DEPTH_TEST);
         id = glGenFramebuffers();
-
-        texture = new Texture(GlobalModules.getWindow().getWidth(), GlobalModules.getWindow().getHeight());
-
-
         glBindFramebuffer(GL_FRAMEBUFFER, id);
+
+        //COLOUR STUFF
         GL11.glDrawBuffer(GL_COLOR_ATTACHMENT0);
-        glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, texture.getId(), 0);
+        colourTexture = new Texture(GlobalModules.getWindow().getWidth(), GlobalModules.getWindow().getHeight());
 
+        glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, colourTexture.getId(), 0);
 
-
-
+        //create Depth
+        depthTexture = createDepthTexture(GlobalModules.getWindow().getWidth(), GlobalModules.getWindow().getHeight());
+        depthBuffer = createDepthBuffer(GlobalModules.getWindow().getWidth(), GlobalModules.getWindow().getHeight());
         if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
             throw new Exception("Could not create FrameBuffer");
         }
@@ -64,8 +64,34 @@ public class FBO {
 
         shaderHandler = new FBOShaderHandler();
         shaderHandler.setupUniforms();
-        mesh = new FBOMesh(pos, textureCoords, indices, texture);
+        mesh = new FBOMesh(pos, textureCoords, indices, colourTexture);
 
+    }
+
+
+    private int createDepthTexture(int width, int height)
+    {
+        int texture = GL11.glGenTextures();
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL14.GL_DEPTH_COMPONENT32, width, height, 0, GL11.GL_DEPTH_COMPONENT, GL11.GL_FLOAT, (ByteBuffer) null);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GL32.glFramebufferTexture(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT, texture, 0);
+        return texture;
+    }
+
+    private int createDepthBuffer(int width, int height){
+        int depthBuffer = GL30.glGenRenderbuffers();
+        GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, depthBuffer);
+        GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL11.GL_DEPTH_COMPONENT, width,
+                height);
+        GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT,
+                GL30.GL_RENDERBUFFER, depthBuffer);
+        return depthBuffer;
+    }
+
+    public int getDepthTexture() {
+        return depthTexture;
     }
 
     public void render(){
@@ -80,8 +106,8 @@ public class FBO {
         this.shaderHandler = shaderHandler;
     }
 
-    public Texture getTexture() {
-        return texture;
+    public Texture getColourTexture() {
+        return colourTexture;
     }
 
     public int getId() {
